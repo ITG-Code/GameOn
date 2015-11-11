fb4b8219e3d046080bd702080951cbf2bde0d664
Hannes Kindströmmer <me@brolaugh.com>
2015-11-11 20:01:02 +0100

    Added sound and enemies follow it's path

--- src/com/gameon/hitboxers/Enemy.java
+++ src/com/gameon/hitboxers/Enemy.java
@@ -1,5 +1,6 @@
 package com.gameon.hitboxers;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 public class Enemy extends Hitboxer {
@@ -9,28 +10,45 @@ public class Enemy extends Hitboxer {
 		super(image, da);
 		x = 600;
 		y = 20;
+		 
 		if(lane == 0){
 			this.setLane(-1);
+			
+			
 		}
 		else if(lane == 1){
 			this.setLane(1);
+			
 		}
 		else if(lane == 2){
 			this.setLane(3);
+			
 		}
 		this.type = type;
 	}
-
+	
+	
 	public void move(double amount) {
-		this.y += amount;
+		int grad = 0;
+        if(getLane() == 3)
+                grad = 229;
+        if(getLane() == 1)
+                grad = 270;
+        if (getLane()== -1)
+                grad = 280;
+		 this.x -= 3 * Math.cos(Math.toRadians(grad));
+		 this.y -= 3 * Math.sin(Math.toRadians(grad));
+		
 	}
 
 	public boolean hit() {
 		return true;
 	}
-	/*public void draw(Graphics2D g){
-		g.rotate(Math.toRadians(30), width / 2, height / 2);
-	}*/
+	public void draw(Graphics2D g){
+		
+		g.drawImage(getImage(),x,y, width/3 + y/6, height/3 + y/6, null);
+	}
+	
 
 	public int getType() {
 		return type;
